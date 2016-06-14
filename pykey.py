52b8b77afa734dfcf31b741c52269955839404d0
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2016-06-14 18:18:44 -0500

    Add diagonal keys, add keys with ctrl combination

--- pykey.py
+++ pykey.py
@@ -13,8 +13,13 @@ key_codes = {
     26: 111, # e
     39: 113, # s
     40: 116, # d
-    41: 114  # f
+    41: 114, # f
+    25: 79,  # w
+    27: 81,  # r
+    53: 87,  # x
+    55: 89   # v
 }
+ctrl_codes = [26,39,40,41]
 KEY_SWITCHER = 36 # key for switching on-off the key remaping
 detection_enabled = True
 
@@ -24,10 +29,14 @@ def handle_event(event):
     if window.get_wm_name() == window_name:
         if event.type == X.KeyRelease and event.state == 4 and event.detail == KEY_SWITCHER:
             detection_enabled = not detection_enabled
-        if event.detail in key_codes.keys() and detection_enabled:
-            handle_keys(event, key_codes[event.detail], event.state)
+        if event.detail in ctrl_codes and detection_enabled and event.state == 4:
+            send_press_key(key_codes[event.detail], event.state)
+            send_release_key(key_codes[event.detail], event.state)
         else:
-            handle_keys(event, event.detail, event.state)
+            if event.detail in key_codes.keys() and detection_enabled:
+                handle_keys(event, key_codes[event.detail], event.state)
+            else:
+                handle_keys(event, event.detail, event.state)
     else:
         handle_keys(event, event.detail, event.state)
 
@@ -72,11 +81,16 @@ def main():
     # we tell the X server we want to catch keyPress event
     root.change_attributes(event_mask = X.KeyPressMask|X.KeyReleaseMask)
     # just grab the "1"-key for now
+    # Common keys without state
     for key_code in key_codes.keys():
         root.grab_key(key_code, 0, True,X.GrabModeSync, X.GrabModeSync)
+    # Keys with shift
+    for key_code in ctrl_codes:
+        root.grab_key(key_code, 4, True,X.GrabModeSync, X.GrabModeSync)
+    # Switcher key
     root.grab_key(KEY_SWITCHER, 4, True,X.GrabModeSync, X.GrabModeSync)
     signal.signal(signal.SIGALRM, lambda a,b:sys.exit(1))
-    signal.alarm(10)
+    signal.alarm(30)
     while 1:
         event = display.next_event()
         handle_event(event)
